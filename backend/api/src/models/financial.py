abf24a9effcdd79164f276acdbb9b106ed83469e
kyle-cassidy <kiccassidy@icloud.com>
2024-02-17 18:07:17 -0500

    Refactored the `Financial` class into `IncomeStatement`, `BalanceSheet`, and `CashFlowStatement` classes with their respective columns.

--- backend/api/src/models/financial.py
+++ backend/api/src/models/financial.py
@@ -1,8 +1,29 @@
-
 # FIXME refactor into the income statement, balance sheet, and cash flow statement
-class Financial:
-    __table__ = 'financials'
-    columns = ['financial_id', 'company_id', 'period', 'revenue', 'net_income', 'eps', 'total_debt', 'total_equity', 'reported_currency']
+class IncomeStatement:
+    __table__ = 'income_statements'
+    columns = ['financial_id', 'company_id', 'period', 'revenue', 'net_income', 'eps', 'reported_currency']
+
+    def __init__(self, **kwargs):
+        for key in kwargs.keys():
+            if key not in self.columns:
+                raise ValueError(f'{key} not in {self.columns}')
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+class BalanceSheet:
+    __table__ = 'balance_sheets'
+    columns = ['financial_id', 'company_id', 'period', 'total_debt', 'total_equity', 'reported_currency']
+
+    def __init__(self, **kwargs):
+        for key in kwargs.keys():
+            if key not in self.columns:
+                raise ValueError(f'{key} not in {self.columns}')
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+class CashFlowStatement:
+    __table__ = 'cash_flow_statements'
+    columns = ['financial_id', 'company_id', 'period', 'operating_activities', 'investing_activities', 'financing_activities', 'reported_currency']
 
     def __init__(self, **kwargs):
         for key in kwargs.keys():
