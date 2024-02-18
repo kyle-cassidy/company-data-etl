e73b8687b1d38c112a4d1d4c129b253f0a1d2ef4
kyle-cassidy <kiccassidy@icloud.com>
2024-02-18 17:11:55 -0500

    all client endpoints are online and producing valid json

--- backend/console.py
+++ backend/console.py
@@ -1,1 +1,18 @@
+from api.src.clients.fmp_client import FMPClient
 
+client = FMPClient()
+
+# these work
+print(client.request_company_profile('AAPL'))
+print(client.request_stock_list())
+print(client.request_all_countries())
+print(client.request_stock_screener('Technology', 'Software - Infrastructure'))
+print(client.request_income_statement('AAPL'))
+print(client.request_balance_sheet('AAPL'))
+print(client.request_cash_flow_statement('AAPL'))
+print(client.request_income_statement_as_reported('AAPL'))
+print(client.request_historical_stock_prices('AAPL'))
+
+# these may be defunct
+# print(client.request_institutional_stock_ownership('AAPL'))
+# print(client.request_analyst_stock_recommendations('AAPL'))
